3a8be6d6d288776e9bdc34413a8253fc79eeba83
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-12 14:23:36 -0500

    Transfer websocket tto its own folder

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { mockAction } from '../../actions';
 import { connect } from 'react-redux';
+import subscribeToTimer from '../../websocket-api';
 
 function App(props) {
+
+  subscribeToTimer((err, timestamp) => props.test(timestamp));
+  
   return (
     <div className="App">
       <header className="App-header">
--- src/reducers/mock.js
+++ src/reducers/mock.js
@@ -1,4 +1,5 @@
 export const mock = (state = [], action) => {
+  console.log(action);
   return action.type === 'MOCK_ACTION'
     ? [...state, { newData: action.data }]
     : state;
