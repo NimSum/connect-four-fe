11cdd5284598c16b314287dc654ed31d036dda92
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-14 14:10:55 -0500

    Set return values for chat, messages, global player updates

--- src/reducers/worldChat.js
+++ src/reducers/worldChat.js
@@ -3,9 +3,12 @@ export const worldChat = (state = { players: [], messages: [] } , action) => {
 
   switch(action.type) {
     case 'SET_GLOBAL_PLAYERS':
+      return { players: action.players, messages };
     case 'UPDATE_GLOBAL_PLAYERS':
     case 'UPDATE_GLOBAL_MESSAGES':
+      return { players, messages: [...messages, action.message] };
     case 'UPDATE_GLOBAL_CHAT':
+      return { players: action.players, messages: action.messages };
     default:
       return state;
   }
