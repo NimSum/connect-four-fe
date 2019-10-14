c4a6a5b255d637d13ecc640ae60019b3a0d793bb
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-14 16:18:58 -0500

    Create play game route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import WorldChat from '../WorldChat';
+import Connect4Grid from '../Connect4Grid';
 import { connect } from 'react-redux';
 
 function App(props) {
@@ -17,7 +18,15 @@ function App(props) {
           return (
             <div>
               <WorldChat />
-              
+            </div>)
+        })}
+      />
+      <Route 
+        exact path='/play_game'
+        render={(() => {
+          return (
+            <div>
+              <Connect4Grid />
             </div>)
         })}
       />
