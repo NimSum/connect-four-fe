ae540010e3ef5353a4f3442a5cb529e528a67e2f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-14 14:15:38 -0500

    Create set  global players action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,9 @@ export const updateGlobalPlayers = player => ({
   type: 'UPDATE_GLOBAL_PLAYERS',
   player
 });
+
+export const setGlobalPlayers = players => ({
+  type: 'SET_GLOBAL_PLAYERS',
+  players
+})
+
