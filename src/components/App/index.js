3eb7ca20470ff666f0417bc9102e47065833a583
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-14 14:19:14 -0500

    Remove mock action test code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import { mockAction } from '../../actions';
+import WorldChat from '../WorldChat';
 import { connect } from 'react-redux';
-import subscribeToTimer from '../../websocket-api';
 
 function App(props) {
 
-  subscribeToTimer((err, timestamp) => props.test(timestamp));
-  
   return (
     <div className="App">
       <header className="App-header">
         Connect Four!
         <button onClick={() => props.test('HI')}>TEST</button>
       </header>
+      <WorldChat />
     </div>
   );
 }
 
 const mapDispatchToProps = dispatch => ({
-  test: data => dispatch(mockAction(data))
 });
 
 export default connect(null, mapDispatchToProps)(App);
