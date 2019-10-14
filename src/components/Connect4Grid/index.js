01967506824cc747703a02e609cbd420dc8b8dfb
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-14 16:05:02 -0500

    Connect gridslots redux state

--- src/components/Connect4Grid/index.js
+++ src/components/Connect4Grid/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-function Connect4Grid() {
+function Connect4Grid(props) {
+  
   return (
     <div>
       
     </div>
   )
-}
+};
 
-export default Connect4Grid;
+const mapStateToProps = ({ gridSlots }) => ({
+  slots: gridSlots
+})
+
+export default connect(mapStateToProps)(Connect4Grid);
