5351cfc8392a78b03255741677cbdb6ccde51d06
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-15 15:07:07 -0500

    Create functional state and render error/loading indicators

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,10 +3,21 @@ import { connect } from 'react-redux';
 import request from '../../requests';
 
 function Login(props) {
+  const [email, setEmail] = useState('nim@sum.com');
+  const [password, setPassword] = useState('nimdimsum');
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState({});
 
   return (
     <div>
-
+      <input type="email" 
+        onChange={({target}) => setEmail(target.value)} 
+        placeholder={email}/>
+      <input type="text" 
+        onChange={({target}) => setPassword(target.value)} 
+        placeholder={password}/>
+      <h4>{isLoading && 'Logging in...'}</h4>
+      <h4>{error.error && error.error}</h4>
     </div>
   )
 };
