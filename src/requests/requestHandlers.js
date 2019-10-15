b8cb746a06965ac84bb189091c55dd9a12cc3d64
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-15 15:14:36 -0500

    Create post method to post payloads based on url

--- src/requests/requestHandlers.js
+++ src/requests/requestHandlers.js
@@ -2,6 +2,13 @@ const handlers = {
   routes:  {
     login: 'http://localhost:3000/api/v1/login',
     signup: 'http://localhost:3000/api/v1/signup',
+  },
+  noTokenPost: function(route, payload) {
+    return fetch(route, {
+      method: 'POST',
+      body: JSON.stringify(payload),
+      headers: { "Content-Type": "application/json" }
+    });
   }
 };
 
