a6fc5ae013ababa72d32bde28326493e73faf762
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-15 15:13:04 -0500

    Create request route handler and request types

---
+++ src/requests/index.js
@@ -0,0 +1,10 @@
+import handler from './requestHandlers';
+
+export default function(type, payload) {
+  switch(type) {
+    case 'login':
+    case 'tokenLogin':
+    case 'signupUser':
+    default:
+  }
+}
