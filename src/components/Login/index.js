f0fb7741cab0e22c9d0301ddb93336c730ed8c69
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-15 15:05:43 -0500

    Create connected login component

---
+++ src/components/Login/index.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import request from '../../requests';
+
+function Login(props) {
+
+  return (
+    <div>
+
+    </div>
+  )
+};
+
+const mapDispatchToProps = state => ({
+
+});
+
+export default connect(mapDispatchToProps)(Login);
