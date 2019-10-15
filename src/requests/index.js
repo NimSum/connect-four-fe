0ef99f1af7a71dee122546c444891c7fd0cb14fc
Nimrod Garcia <nimm.garcia@gmail.com>
2019-10-15 15:21:32 -0500

    Create request executions

--- src/requests/index.js
+++ src/requests/index.js
@@ -3,8 +3,15 @@ import handler from './requestHandlers';
 export default function(type, payload) {
   switch(type) {
     case 'login':
+      return handler.loginViaEmail(payload);
     case 'tokenLogin':
+      const token = JSON.parse(localStorage.getItem('user_token')) || payload;
+      handler.loginViaToken(token);
+      break;
     case 'signupUser':
+      handler.signupUser(payload);
+      break;
     default:
+      console.log('def');
   }
 }
