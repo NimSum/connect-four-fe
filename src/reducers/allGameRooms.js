fc837dce5ae28fd81969d67988892cd5f8d44533
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 15:51:20 -0500

    Create all rooms setup and user logout cases

--- src/reducers/allGameRooms.js
+++ src/reducers/allGameRooms.js
@@ -1,5 +1,12 @@
 const allGameRooms = (state = [], action) => {
+  const rooms = [...state];
+
   switch(action.type) {
+    case 'SET_ALL_GAME_ROOMS':
+      return  action.room;
+    case 'UPDATE_GAME_ROOM_LIST':
+    case 'LOGOUT_USER':
+      return [];
     default:
       return state;
   }
