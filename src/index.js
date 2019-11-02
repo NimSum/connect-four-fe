0cb6c3ed4cde1cea421f8def605646ce59c9cf8f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 16:08:39 -0500

    Update import order

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './components/App';
+import rootReducer from './reducers';
+import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
-import * as serviceWorker from './serviceWorker';
-import './index.scss';
 
 const store = createStore(rootReducer, composeWithDevTools());
 
