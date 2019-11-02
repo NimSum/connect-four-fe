7d3bf31814effca9de4f3c7eaef4fcba09834ecc
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 15:40:45 -0500

    Import MUI components and needed packages

---
+++ src/components/WorldChat/WorldChatPlayers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { Divider } from '@material-ui/core';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import generateAvatar from '../../utils/nameAvatarGenerator';
+import theme from '../../MUI_theme';
+import uuidv4 from 'uuid/v4';
