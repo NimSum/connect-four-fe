e1f8b74c2fed48737671dad56a5459f48ea6f8a7
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 15:45:14 -0500

    Create game rooms actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,7 +19,26 @@ export const logoutUser = () => ({
   type: 'LOGOUT_USER'
 })
 
+/// PLAYER INFO
 export const saveCurrentPlayer = player => ({
   type: 'SAVE_PLAYER_INFO',
   player
 })
+
+
+/// GAME ROOMS ACTIONS
+export const setAllGameRooms = room => ({
+  type: 'SET_ALL_GAME_ROOMS',
+  room
+})
+
+export const updateGameRoomList = room => ({
+  type: 'UPDATE_GAME_ROOM_LIST',
+  room
+})
+
+export const updateGameRoom = room => ({
+  type: 'UPDATE_A_GAME_ROOM',
+  room
+})
+
