527ebe0fe70d9a639ae6a51166bc5ad137e7d520
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 16:14:21 -0500

    Create default component  states and style args

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -5,6 +5,30 @@ import Box from '@material-ui/core/Box';
 import Grow from '@material-ui/core/Grow';
 
 function Splash() {
+  const randomizedColors = Object.values(theme.palette.chips)
+    .sort(() => .5 - Math.random());
+  const [chipOne] = useState(randomizedColors[0]);
+  const [chipTwo] = useState(randomizedColors[1]);
+  const [currSlot, setCurrSlot] = useState([0, 0]);
+  const [currChip, setCurrChip] = useState(1);
+  const [grid, setGrid] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2 ], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+    [0, 0, 0, 0, 0, 0, 0, 1, 2, 2 ], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 2, 2 ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1 ], 
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 2 ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 2 ], 
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+  ]);
+  const classes = useStyles({
+    ...theme,
+    chipOne,
+    chipTwo
+  });
   return (
     <Box className={classes.root}>
       <Box boxShadow={3} className={classes.gridContainer}>
