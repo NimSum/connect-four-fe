f29b1efac81c80bcbf268e3bfafc7d0fe746f993
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 15:51:50 -0500

    Create room list update & individual room update cases

--- src/reducers/allGameRooms.js
+++ src/reducers/allGameRooms.js
@@ -5,6 +5,22 @@ const allGameRooms = (state = [], action) => {
     case 'SET_ALL_GAME_ROOMS':
       return  action.room;
     case 'UPDATE_GAME_ROOM_LIST':
+      const { updateType, roomId } = action.room;
+      if (updateType === 'addRoom') {
+        return [ action.room, ...state ];
+      } else if (updateType === 'deleteRoom') {
+        const idxFound = state.findIndex(r => r.roomId === roomId);
+        rooms.splice(idxFound, 1);
+        return rooms;
+      };
+      break;
+    case 'UPDATE_A_GAME_ROOM':
+      if (action.room.updateType === 'updateRoom') {
+        const idxFound = state.findIndex(r => r.roomId === action.room.roomId);
+        rooms.splice(idxFound, 1, action.room);
+        return rooms;
+      };
+      break;
     case 'LOGOUT_USER':
       return [];
     default:
