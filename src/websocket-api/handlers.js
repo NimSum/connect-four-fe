1fc1a720fd7ef966a05f046983bef9927b2d990a
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 16:02:29 -0500

    Create all game rooms handler

--- src/websocket-api/handlers.js
+++ src/websocket-api/handlers.js
@@ -13,7 +13,7 @@ import {
 } from '../actions';
 import store from '../index';
 
-
+//// WORLD CHAT
 export function handleWorldChat(data) {
   const { type } = data;
   if (type === 'player') {
@@ -27,17 +27,12 @@ export function handleAllWorldPlayers(players) {
   store.dispatch(setGlobalPlayers(players));
 }
 
-export function handleGameUpdate(data) {
-  // store.dispatch(updateGridSlots(data));
-  const grid = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 0, 0, 0]
-  ];
-  const currentPlayer = 1;
+
+/// GAME ROOM HANDLERS
+export function handleAllGameRooms(rooms) {
+  store.dispatch(setAllGameRooms(rooms));
+} 
+
   
   store.dispatch(updateGridSlots({currentPlayer, grid}));
 };
