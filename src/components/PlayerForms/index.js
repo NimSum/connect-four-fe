7c67a8b9dd0c2ccc96b6e29291ba0d59eb6d0ab7
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-02 19:14:23 -0500

    Import components and create componend base

---
+++ src/components/PlayerForms/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import request from '../../requests';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Grow from '@material-ui/core/Grow';
+import { makeStyles } from '@material-ui/core/styles';
+
+function PlayerForms(props) {
+
+  return (
+    <div className={classes.root}>
+    </div>
+  );
+};
+
+export default PlayerForms;
