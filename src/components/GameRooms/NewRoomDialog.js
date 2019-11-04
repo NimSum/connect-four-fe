8883e60862c1a92e4ff6065bbad8b2cda25c0279
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-04 11:10:52 -0600

    Import MUI components and create base component base

---
+++ src/components/GameRooms/NewRoomDialog.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default function NewRoomDialog({submitHandler, isVisible, showForm }) {
+  return (
+    <Dialog open={isVisible} onClose={() => showForm(false)} aria-labelledby="Create new room">
+      <DialogTitle id="Create new room">Create New Room</DialogTitle>
+    </Dialog>
+  );
+}
