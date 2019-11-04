d137529758af7fa967a683ef2e947d81d2b7df01
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-04 12:09:06 -0600

    Import components, map state and dispatch and deconstruct props

---
+++ src/components/ActiveGame/GameGrid.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from 'react';
+import theme from '../../MUI_theme';
+import Box from '@material-ui/core/Box';
+import Fade from '@material-ui/core/Fade';
+import Grow from '@material-ui/core/Grow';
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import WaitingRoom from '../WaitingRoom';
+import { rematchReset, resetReady } from '../../actions';
+import { connect } from 'react-redux';
+import { placePlayerChip } from '../../websocket-api';
+import { makeStyles } from '@material-ui/core/styles';
+
+function GameGrid(props) {
+  const { 
+    grid, 
+    gameStatus, 
+    currentTurn, 
+    currentPlayer, 
+    players, 
+    prevSlot, 
+    winner, 
+    resetGame, 
+    resetPlayerReady
+  } = props;
+  return (
+    <div className={classes.root}>
+    </div>
+  )
+};
+const mapStateToProps = ({ currentGame, currentPlayer }) => ({
+  grid: currentGame.gridX,
+  currentTurn: currentGame.currentPlayer
+    ? currentGame.currentPlayer.player_name
+    : currentGame.currentPlayer,
+  currentPlayer: currentPlayer.player
+    ? currentPlayer.player.player_name
+    : null,
+  players: currentGame.players,
+  prevSlot: currentGame.prevSlot,
+  winner: currentGame.winner,
+  gameStatus: currentGame.status
+});
+
+const mapDispatchToProps = dispatch => ({
+  resetGame: () => dispatch(rematchReset()),
+  resetPlayerReady: () => dispatch(resetReady())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameGrid);
