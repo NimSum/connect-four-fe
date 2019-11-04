4d1b1928277ff2b0f7a5290ab7b8fab33dd42aa4
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-04 14:28:39 -0600

    Updates

---
+++ precache-manifest.fea84c770d15c2716b4ac220363a72ab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4b996da1e3b3a8f2302b15d78ef2ad",
+    "url": "/connect-four-fe/index.html"
+  },
+  {
+    "revision": "fc41c6e83013a7a78b38",
+    "url": "/connect-four-fe/static/js/2.02f7b2ab.chunk.js"
+  },
+  {
+    "revision": "1a8a0c281147ad10cbf2",
+    "url": "/connect-four-fe/static/js/main.3e47c925.chunk.js"
+  },
+  {
+    "revision": "1b76cb6bd17d6afe2170",
+    "url": "/connect-four-fe/static/js/runtime-main.6c4f924e.js"
+  }
+]);
