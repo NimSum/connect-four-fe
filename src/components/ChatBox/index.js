f2dc3e74cedce8a780074aec84583ce8531cd86b
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-04 11:41:54 -0600

    Import MUI components and create base component

---
+++ src/components/ChatBox/index.js
@@ -0,0 +1,22 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Send from '@material-ui/icons/Send';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import theme from '../../MUI_theme';
+import uuidv4 from 'uuid/v4';
+
+function ChatBox(props) {
+  const classes = useStyles(theme);
+  return (
+    <Box boxShadow={3} className={classes.root}>
+    </Box>
+  );
+}
+
+export default ChatBox;
