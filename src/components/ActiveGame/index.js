4c8d1fe54566fa4f18ec92f2b78c7b3431ead338
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-04 12:40:36 -0600

    Style container and toggle

--- src/components/ActiveGame/index.js
+++ src/components/ActiveGame/index.js
@@ -10,6 +10,42 @@ import { connect } from 'react-redux';
 import { sendInGameMessage } from '../../websocket-api';
 import { leaveActiveGame, resetGrid } from '../../actions';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    maxWidth: '1600px',
+    padding: theme.spacing(1),
+    [theme.breakpoints.up('lg')]: {
+      margin: '0 auto'
+    }, 
+    [theme.breakpoints.down('md')]: {
+      flexWrap: 'wrap',
+      justifyContent: 'center'
+    },
+    background: ({palette}) => palette.dark.midDarkBlue,
+  },
+  chatBox: {
+    [theme.breakpoints.down('md')]: {
+      marginTop: theme.spacing(2),
+      width: '70vw',
+      height: '60vh'
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '100%'
+    },
+    display: 'flex',
+    width: '100%',
+    position: 'relative',
+  },
+  chatToggle: {
+    position: 'absolute',
+    right: '5px',
+    top: '5px',
+    zIndex: 1
+  }
+
+}));
+
 function ActiveGame(props) {
   const classes = useStyles(theme);
   const [isChatActive, setChatActive] = useState(true);
