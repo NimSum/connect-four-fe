1e1d2e9f957facf60bc2ba38784cc2675baf0f34
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-14 16:29:26 -0600

    Create PlayerDetails basic component structure

---
+++ src/components/PlayerDrawer/PlayerDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import ListItem from '@material-ui/core/ListItem';
+import Grow from '@material-ui/core/Grow';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+
+function PlayerDetails({player}) {
+  return (
+  )
+}
+
+export default PlayerDetails
