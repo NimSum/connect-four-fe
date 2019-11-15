077739db2c715256da6042d6f702937981e5a27f
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-15 15:35:24 -0600

    Remove comments, prettyfies code

--- src/websocket-api/handlers.js
+++ src/websocket-api/handlers.js
@@ -1,66 +1,58 @@
-import { 
-  updateGlobalPlayers,
-  updateActiveGame,
-  updateGlobalMessages,
-  setGlobalPlayers,
-  setAllGameRooms,
-  updateGameRoomList,
-  updateGameRoom,
-  updateActiveGameMsgs,
-  updateInactiveGame,
-  gameOver,
-  logoutUser 
-} from '../actions';
-import store from '../index';
+import {
+	updateGlobalPlayers,
+	updateActiveGame,
+	updateGlobalMessages,
+	setGlobalPlayers,
+	setAllGameRooms,
+	updateGameRoomList,
+	updateGameRoom,
+	updateActiveGameMsgs,
+	updateInactiveGame,
+	gameOver,
+	logoutUser
+} from "../actions";
+import store from "../index";
 
-//// WORLD CHAT
 export function handleWorldChat(data) {
-  const { type } = data;
-  if (type === 'player') {
-    store.dispatch(updateGlobalPlayers(data));
-  } else if (type === 'message'  || type === 'notification') {
-    store.dispatch(updateGlobalMessages(data));
-  }
-};
+	const { type } = data;
+	if (type === "player") {
+		store.dispatch(updateGlobalPlayers(data));
+	} else if (type === "message" || type === "notification") {
+		store.dispatch(updateGlobalMessages(data));
+	}
+}
 
 export function handleAllWorldPlayers(players) {
-  store.dispatch(setGlobalPlayers(players));
+	store.dispatch(setGlobalPlayers(players));
 }
 
-
-/// GAME ROOM HANDLERS
 export function handleAllGameRooms(rooms) {
-  store.dispatch(setAllGameRooms(rooms));
-} 
+	store.dispatch(setAllGameRooms(rooms));
+}
 
 export function handleGameRoomUpdate(update) {
-  const { updateType } = update;
-  if (['addRoom', 'deleteRoom'].includes(updateType)) {
-    store.dispatch(updateGameRoomList(update));
-  } else if (updateType === 'updateRoom') {
-    store.dispatch(updateGameRoom(update))
-  }
+	const { updateType } = update;
+	if (["addRoom", "deleteRoom"].includes(updateType)) {
+		store.dispatch(updateGameRoomList(update));
+	} else if (updateType === "updateRoom") {
+		store.dispatch(updateGameRoom(update));
+	}
 }
 
-
-
-//// ACTIVE GAME
 export function handleActiveGameUpdate(data) {
-  const {type} = data;
+	const { type } = data;
 
-  if (type === 'message' || type === 'notification') {
-    store.dispatch(updateActiveGameMsgs(data));
-  } else if (type === 'inactiveUpdate') {
-    store.dispatch(updateInactiveGame(data));
-  } else if (type === 'activeUpdate') {
-    store.dispatch(updateActiveGame(data));
-  } else if (type === 'gameOver') {
-    store.dispatch(gameOver(data));
-  }
-};
+	if (type === "message" || type === "notification") {
+		store.dispatch(updateActiveGameMsgs(data));
+	} else if (type === "inactiveUpdate") {
+		store.dispatch(updateInactiveGame(data));
+	} else if (type === "activeUpdate") {
+		store.dispatch(updateActiveGame(data));
+	} else if (type === "gameOver") {
+		store.dispatch(gameOver(data));
+	}
+}
 
-
-//// OTHER
 export function handleDisconnect() {
-  store.dispatch(logoutUser());
+	store.dispatch(logoutUser());
 }
