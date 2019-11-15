083d72f146503d855baec898c71ed452bd14f2a8
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-15 15:36:41 -0600

    Prettyfies code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import App from './components/App';
-import rootReducer from './reducers';
-import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import App from "./components/App";
+import rootReducer from "./reducers";
+import * as serviceWorker from "./serviceWorker";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <Provider store={store}>
-    <HashRouter>
-      <CssBaseline />
-      <App />
-    </HashRouter>
-  </Provider>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<HashRouter>
+			<CssBaseline />
+			<App />
+		</HashRouter>
+	</Provider>,
+	document.getElementById("root")
 );
 
 serviceWorker.unregister();
