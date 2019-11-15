25b9d875ae2d98b228fec0a5e299476983b37d3a
Nimrod Garcia <nimm.garcia@gmail.com>
2019-11-15 15:35:59 -0600

    Remove comments, prettyfies code

--- src/websocket-api/index.js
+++ src/websocket-api/index.js
@@ -1,87 +1,78 @@
-import openSocket from 'socket.io-client';
+import openSocket from "socket.io-client";
 import {
-  handleWorldChat,
-  handleAllWorldPlayers,
-  handleAllGameRooms,
-  handleGameRoomUpdate,
-  handleActiveGameUpdate,
-  handleDisconnect
-} from './handlers';
+	handleWorldChat,
+	handleAllWorldPlayers,
+	handleAllGameRooms,
+	handleGameRoomUpdate,
+	handleActiveGameUpdate,
+	handleDisconnect
+} from "./handlers";
 
-const socket = openSocket('https://connect-four-be.herokuapp.com/');
+const socket = openSocket("https://connect-four-be.herokuapp.com/");
 
-//// AUTO WORLD CHAT 
-socket.on('world chat update', handleWorldChat);
-socket.on('send world chat players', handleAllWorldPlayers);
-//// AUTO GAME ROOMS
-socket.on('send all game rooms', handleAllGameRooms);
-socket.on('game rooms update', handleGameRoomUpdate);
-socket.on('disconnect', handleDisconnect);
+socket.on("world chat update", handleWorldChat);
+socket.on("send world chat players", handleAllWorldPlayers);
 
-/// WORLD CHAT
+socket.on("send all game rooms", handleAllGameRooms);
+socket.on("game rooms update", handleGameRoomUpdate);
+socket.on("disconnect", handleDisconnect);
+
 export function sendWorldMessage(message) {
-  socket.emit('broadcast to world chat', message)
-};
+	socket.emit("broadcast to world chat", message);
+}
 
 export function leaveWorldChat() {
-  socket.emit('leave world chat');
-};
+	socket.emit("leave world chat");
+}
 
 export function joinWorldChat(player) {
-  socket.emit('join world chat', player);
-};
+	socket.emit("join world chat", player);
+}
 
 export function getWorldChatPlayers() {
-  socket.emit('get world chat players');
+	socket.emit("get world chat players");
 }
 
 export function subscribeToWorldChat() {
-  socket.on('world chat update', handleWorldChat);
-};
+	socket.on("world chat update", handleWorldChat);
+}
 
-
-/// CLIENT REGISTRATION
 export function registerClient(token) {
-  socket.emit('register client', token);
+	socket.emit("register client", token);
 }
 
 export function removeClient(token) {
-  socket.emit('remove client', token);
-};
+	socket.emit("remove client", token);
+}
 
+export function createNewGameRoom(name, password = "") {
+	socket.emit("create game room", { name, password });
+}
 
-
-/// GAME ROOMS
-export function createNewGameRoom(name, password = '') {
-  socket.emit('create game room', { name, password });
-};
-
-export function joinGameRoom(roomId, password = '') {
-  socket.emit('join game room', {roomId, password});
-};
+export function joinGameRoom(roomId, password = "") {
+	socket.emit("join game room", { roomId, password });
+}
 
 export function leaveGame() {
-  socket.emit('leave game room');
-};
+	socket.emit("leave game room");
+}
 
-
-//// Active Game
 export function listenToActiveGame() {
-  socket.on('active game update', handleActiveGameUpdate);
-};
+	socket.on("active game update", handleActiveGameUpdate);
+}
 
 export function unsubscribeToActiveGame() {
-  socket.removeListener('active game update')
+	socket.removeListener("active game update");
 }
 
 export function setPlayerReady(data) {
-  socket.emit('set player ready', data);
-};
+	socket.emit("set player ready", data);
+}
 
 export function placePlayerChip(xCoordinate) {
-  socket.emit('place player chip', xCoordinate);
-};
+	socket.emit("place player chip", xCoordinate);
+}
 
 export function sendInGameMessage(payload) {
-  socket.emit('send in game message', payload);
-};
+	socket.emit("send in game message", payload);
+}
